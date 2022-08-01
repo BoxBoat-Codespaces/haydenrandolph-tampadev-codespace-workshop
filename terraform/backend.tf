ccdc187c10d9df705af22a7abfbc0047d48f1ea2
Hayden Randolph <hayden@haydenrandolph.com>
2022-08-01 18:04:30 +0000

    deploying container app

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "codespaces-demo-resources"
-    storage_account_name = "codespacesstate1"
+    resource_group_name  = "codespaces-workshop-state"
+    storage_account_name = "codespacesstate9"
     container_name       = "codespacesstate"
     key                  = "haydenrandolph.state"
   }
